246eaf76ae6c5898cd9d32fb08c04c855b100ea2
Tatiane Moreira <thatiane.moreiraa@gmail.com>
2017-12-12 22:49:23 -0200

    implementação do login com e-mail

--- SharingIOS/LoginController.swift
+++ SharingIOS/LoginController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginController: AutenticationController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func signInButtonClick(_ sender: Any) {
+        if let email = txtEmail.text, let password = txtPassword.text {
+            if (email.isEmpty || password.isEmpty) {
+                showMessage(title: "Atenção", message: "Todos os campos são obrigatórios!")
+            } else {
+                if (isEmailValid(email) ) {
+                    if (isPasswordValid(password)) {
+                        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                            if user != nil {
+                                let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+                                let mainViewController = storyBoard.instantiateInitialViewController()
+                                self.present(mainViewController!, animated:true, completion:nil)
+                            } else {
+                                self.showMessage(title: "Erro", message: error.debugDescription)
+                            }
+                        }
+                    } else {
+                        showMessage(title: "Atenção", message: "A senha deve ter no mínimo 6 caracteres!")
+                    }
+                } else {
+                    showMessage(title: "Atenção", message: "Email inválido!")
+                }
+            }
+        }
     }
-
-
+    
 }
 
